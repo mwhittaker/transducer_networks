02c1409151b9ea60b8d44274f36cb44eebb37049
Michael Whittaker <mjwhittaker@berkeley.edu>
2017-10-28 16:20:53 -0700

    Added JSON functionality.
    Ideally, we could build a javascript visualizer for the networks, but I
    probably won't be doing that any time soon.

--- examples.py
+++ examples.py
@@ -1,3 +1,6 @@
+import argparse
+import json
+
 from schema import TransducerSchema
 from transducer import Transducer
 from transducer_network import TransuducerNetwork
@@ -10,23 +13,28 @@ class Ameloot1(Transducer):
         assert r == "T"
         return {(x, y) for (x, y) in state.in_["R"] if x == y}
 
-def main():
-    A, B, C = "ABC"
-    net = {A: {B, C}, B: {A, C}, C: {A, B}}
-    in_0 = {
-        A: {"R": {(1, 1), (1, 2), (2, 1), (2, 2)}},
-        B: {"R": {(3, 3), (3, 4), (4, 3), (4, 4)}},
-        C: {"R": {(5, 5), (5, 6), (6, 5), (6, 6)}},
-    }
-    tn = TransuducerNetwork(net, Ameloot1(), in_0)
+    @staticmethod
+    def example():
+        A, B, C = "A", "B", "C"
+        net = {A: {B, C}, B: {A, C}, C: {A, B}}
+        in_0 = {
+            A: {"R": {(1, 1), (1, 2), (2, 1), (2, 2)}},
+            B: {"R": {(3, 3), (3, 4), (4, 3), (4, 4)}},
+            C: {"R": {(5, 5), (5, 6), (6, 5), (6, 6)}},
+        }
+        return TransuducerNetwork(net, Ameloot1(), in_0)
 
-    print tn.out()
-    tn.step(1)
-    print tn.out()
-    tn.step(1)
-    print tn.out()
-    tn.step(1)
-    print tn.out()
+def main():
+    examples = {
+        "Ameloot1": Ameloot1().example(),
+    }
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("example", choices=examples.keys())
+    args = parser.parse_args()
+
+    tn = examples[args.example]
+    print(json.dumps(tn.run_json()))
 
 if __name__ == "__main__":
     main()
--- transducer_network.py
+++ transducer_network.py
@@ -4,6 +4,33 @@ import json
 
 from schema import TransducerState, database_satisfies_schema
 
+class Dict:
+    @staticmethod
+    def map(d, f):
+        return {k: f(v) for (k, v) in d.iteritems()}
+
+    @staticmethod
+    def map2(d1, d2, f):
+        assert d1.keys() == d2.keys()
+        return {k: f(d1[k], d2[k]) for k in d1}
+
+def _state_0(node, nodes, schema, in_0):
+    assert database_satisfies_schema(in_0, schema.in_)
+    out = {r: set() for r in schema.out}
+    msg = {r: set() for r in schema.msg}
+    mem = {r: set() for r in schema.mem}
+    sys = {"Id": {node}, "All": nodes}
+    return TransducerState(in_0, out, msg, mem, sys)
+
+def _union_dicts(d1, d2):
+    assert d1.keys() == d2.keys()
+    return {k: d1[k] | d2[k] for k in d1}
+
+def _random_subset(ms):
+    xs = ms.elements()
+    xs = random.sample(xs, random.randint(0, len(xs) - 1))
+    return Counter(xs)
+
 class TransuducerNetwork(object):
     def __init__(self, network, transducer, in_0):
         self._network = network
@@ -11,26 +38,33 @@ class TransuducerNetwork(object):
         self._schema = transducer.schema()
         self._in_0 = in_0
 
+        # Initialize the state and message buffers for each node.
         nodes = set(network.keys())
-        s0 = TransuducerNetwork._state_0
-        self._states = {node: s0(node, nodes, self._schema, in_0[node])
-                        for node in nodes}
-        self._bufs = {node: {r: Counter() for r in self._schema.msg}
-                      for node in nodes}
+        self._states = dict()
+        self._bufs = dict()
+        for node in nodes:
+            self._states[node] = TransducerState(
+                in_0[node],
+                {r: set() for r in self._schema.out},
+                {r: set() for r in self._schema.msg},
+                {r: set() for r in self._schema.mem},
+                {"Id": {node}, "All": nodes},
+            )
+            self._bufs[node] = {r: Counter() for r in self._schema.msg}
 
     def __str__(self):
-        def sets_to_lists(d):
-            return {k: list(v) for (k, v) in d.items()}
+        return json.dumps(self.to_json())
 
-        nodes = {}
+    def to_json(self):
+        data = {}
         for (node, state) in self._states.items():
-            nodes[node] = {
-                "in": sets_to_lists(state.in_),
-                "out": sets_to_lists(state.out),
-                "mem": sets_to_lists(state.mem),
-                "buf": {r: c.elements() for (r, c) in self._bufs[node].items()},
+            data[node] = {
+                "in": Dict.map(state.in_, list),
+                "out": Dict.map(state.out, list),
+                "mem": Dict.map(state.mem, list),
+                "buf": Dict.map(self._bufs[node], lambda c: c.elements())
             }
-        return json.dumps(nodes)
+        return data
 
     def configuration(self):
         return (self._states, self._bufs)
@@ -38,14 +72,22 @@ class TransuducerNetwork(object):
     def out(self):
         out_0 = {r: set() for r in self._schema.out}
         outs = [s.out for s in self._states.values()]
-        return reduce(TransuducerNetwork.union_dicts, outs, out_0)
+        f = lambda d1, d2: Dict.map2(d1, d2, lambda s1, s2: s1 | s2)
+        return reduce(f, outs, out_0)
 
     def step(self):
         node = random.choice(self._network.keys())
         buf = self._bufs[node]
-        msgs = {r: TransuducerNetwork.random_subset(buf[r]) for r in buf}
+        msgs = {r: _random_subset(buf[r]) for r in buf}
         self._step(node, msgs)
         return (node, msgs)
+
+    def step_json(self):
+        node, msgs = self.step()
+        return {
+            "node": node,
+            "msgs": Dict.map(msgs, lambda c: c.elements()),
+        }
 
     def run(self):
         last_out = None
@@ -54,6 +96,20 @@ class TransuducerNetwork(object):
             last_out = out
             self.step()
             out = self.out()
+
+    def run_json(self):
+        data = {"states": [], "steps": []}
+        step = None
+        last_out = None
+        out = self.out()
+        while last_out != out:
+            data["states"].append(self.to_json())
+            if step is not None:
+                data["steps"].append(step)
+            last_out = out
+            step = self.step_json()
+            out = self.out()
+        return data
 
     def _step(self, node, msgs):
         # Sanity check msgs.
@@ -73,23 +129,3 @@ class TransuducerNetwork(object):
         for neighbor in self._network[node]:
             for (r, d) in snd:
                 self._bufs[neighbor][r].update(Counter(d))
-
-    @staticmethod
-    def _state_0(node, nodes, schema, in_0):
-        assert database_satisfies_schema(in_0, schema.in_)
-        out = {r: set() for r in schema.out}
-        msg = {r: set() for r in schema.msg}
-        mem = {r: set() for r in schema.mem}
-        sys = {"Id": {node}, "All": nodes}
-        return TransducerState(in_0, out, msg, mem, sys)
-
-    @staticmethod
-    def union_dicts(d1, d2):
-        assert d1.keys() == d2.keys()
-        return {k: d1[k] | d2[k] for k in d1}
-
-    @staticmethod
-    def random_subset(ms):
-        xs = ms.elements()
-        xs = random.sample(xs, random.randint(0, len(xs) - 1))
-        return Counter(xs)
