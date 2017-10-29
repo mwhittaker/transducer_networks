3f3e4f46a3c599b99efa74d6e357b20603c85233
Michael Whittaker <mjwhittaker@berkeley.edu>
2017-10-29 14:51:46 -0700

    Tidied up + interesting proof.

--- examples.py
+++ examples.py
@@ -1,5 +1,4 @@
-import argparse
-import json
+from collections import Counter
 
 from schema import TransducerSchema
 from transducer import Transducer
@@ -24,17 +23,77 @@ class Ameloot1(Transducer):
         }
         return TransuducerNetwork(net, Ameloot1(), in_0)
 
-def main():
-    examples = {
-        "Ameloot1": Ameloot1().example(),
-    }
+class MonotoneButNotConsistent(Transducer):
+    """
+    This transducer shows that not all monotone transducers are consistent.
+    This transducer is defined over a schema with an I input relation, an O
+    output relation, an A and R message relations, and an M memory relation.
+    All relations have arity 1. We can express each query as a non-recursive
+    Datalog query:
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("example", choices=examples.keys())
-    args = parser.parse_args()
+        # Send input to everyone else.
+        A(a) :- I(a)
+        R(a) :- I(a)
 
-    tn = examples[args.example]
-    print(json.dumps(tn.run_json()))
+        # Update memory.
+        M_ins(a) :- A(a)
+        M_del(a) :- R(a)
 
-if __name__ == "__main__":
-    main()
+        # Output everything in M.
+        O(a) :- M(a)
+
+    Clearly, every rule is monotone. However, we can construct two runs with
+    different quiescent outputs; see the proof method below.
+    """
+    def schema(self):
+        return TransducerSchema(
+            in_={"I": 1},
+            out={"O": 1},
+            msg={"A": 1, "R": 1},
+            mem={"M": 1},
+        )
+
+    def snd(self, r, state):
+        assert r in ["A", "R"]
+        return state.in_["I"]
+
+    def add(self, r, state):
+        assert r == "M"
+        return state.msg["A"]
+
+    def rem(self, r, state):
+        assert r == "M"
+        return state.msg["R"]
+
+    def out(self, r, state):
+        assert r == "O"
+        return state.mem["M"]
+
+    @staticmethod
+    def example():
+        X, Y = "X", "Y"
+        net = {A: {Y}, Y: {A}}
+        in_0 = {A: {"I": {(1,)}}, Y: {"I": {(2,)}}}
+        return TransuducerNetwork(net, MonotoneButNotConsistent(), in_0)
+
+    @staticmethod
+    def proof():
+        # It's easy to construct a run in which the output quiesces at {1, 2}.
+        tn = MonotoneButNotConsistent.example()
+        tn.step("X", {"A": dict(), "R": dict()})
+        tn.step("Y", {"A": dict(), "R": dict()})
+        tn.step("X", {"A": Counter([(2,)]), "R": dict()})
+        tn.step("X", {"A": dict(), "R": dict()})
+        tn.step("Y", {"A": Counter([(1,)]), "R": dict()})
+        tn.step("Y", {"A": dict(), "R": dict()})
+        print tn.out()
+
+        # We can also construct an infinite run in which nothing is output. We
+        # just have to make sure to always deliver the A and R facts together.
+        tn = MonotoneButNotConsistent.example()
+        tn.step("X", {"A": dict(), "R": dict()})
+        # Imagine this ran forever!
+        for _ in range(100):
+            tn.step("Y", {"A": Counter([(1,)]), "R": Counter([(1,)])})
+            tn.step("X", {"A": Counter([(2,)]), "R": Counter([(2,)])})
+        print tn.out()
--- transducer_network.py
+++ transducer_network.py
@@ -14,21 +14,9 @@ class Dict:
         assert d1.keys() == d2.keys()
         return {k: f(d1[k], d2[k]) for k in d1}
 
-def _state_0(node, nodes, schema, in_0):
-    assert database_satisfies_schema(in_0, schema.in_)
-    out = {r: set() for r in schema.out}
-    msg = {r: set() for r in schema.msg}
-    mem = {r: set() for r in schema.mem}
-    sys = {"Id": {node}, "All": nodes}
-    return TransducerState(in_0, out, msg, mem, sys)
-
-def _union_dicts(d1, d2):
-    assert d1.keys() == d2.keys()
-    return {k: d1[k] | d2[k] for k in d1}
-
 def _random_subset(ms):
-    xs = ms.elements()
-    xs = random.sample(xs, random.randint(0, len(xs) - 1))
+    xs = list(ms.elements())
+    xs = random.sample(xs, random.randint(0, len(xs)))
     return Counter(xs)
 
 class TransuducerNetwork(object):
@@ -62,7 +50,7 @@ class TransuducerNetwork(object):
                 "in": Dict.map(state.in_, list),
                 "out": Dict.map(state.out, list),
                 "mem": Dict.map(state.mem, list),
-                "buf": Dict.map(self._bufs[node], lambda c: c.elements())
+                "buf": Dict.map(self._bufs[node], lambda c: list(c.elements()))
             }
         return data
 
@@ -75,43 +63,7 @@ class TransuducerNetwork(object):
         f = lambda d1, d2: Dict.map2(d1, d2, lambda s1, s2: s1 | s2)
         return reduce(f, outs, out_0)
 
-    def step(self):
-        node = random.choice(self._network.keys())
-        buf = self._bufs[node]
-        msgs = {r: _random_subset(buf[r]) for r in buf}
-        self._step(node, msgs)
-        return (node, msgs)
-
-    def step_json(self):
-        node, msgs = self.step()
-        return {
-            "node": node,
-            "msgs": Dict.map(msgs, lambda c: c.elements()),
-        }
-
-    def run(self):
-        last_out = None
-        out = self.out()
-        while last_out != out:
-            last_out = out
-            self.step()
-            out = self.out()
-
-    def run_json(self):
-        data = {"states": [], "steps": []}
-        step = None
-        last_out = None
-        out = self.out()
-        while last_out != out:
-            data["states"].append(self.to_json())
-            if step is not None:
-                data["steps"].append(step)
-            last_out = out
-            step = self.step_json()
-            out = self.out()
-        return data
-
-    def _step(self, node, msgs):
+    def step(self, node, msgs):
         # Sanity check msgs.
         buf = self._bufs[node]
         assert all(0 <= msgs[r] <= buf[r] for r in self._schema.msg)
@@ -123,9 +75,16 @@ class TransuducerNetwork(object):
         self._states[node] = new_state
 
         # Update message buffers.
-        for (r, d) in msgs:
+        for (r, d) in msgs.items():
             buf[r].subtract(d)
 
         for neighbor in self._network[node]:
-            for (r, d) in snd:
+            for (r, d) in snd.items():
                 self._bufs[neighbor][r].update(Counter(d))
+
+    def random_step(self):
+        node = random.choice(self._network.keys())
+        buf = self._bufs[node]
+        msgs = {r: _random_subset(buf[r]) for r in buf}
+        self.step(node, msgs)
+        return (node, msgs)
